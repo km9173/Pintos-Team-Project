2d09eff2f98011f0c8d1bf2761dd9b805433af5b
GyeongMo Lim <km9173@naver.com>
2016-03-05 00:18:41 +0900

    add get program name in command line

--- userprog/process.c
+++ userprog/process.c
@@ -26,11 +26,16 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
 tid_t
-process_execute (const char *file_name) 
+process_execute (const char *file_name)
 {
   char *fn_copy;
+  char *name;
+  char *saveptr;
   tid_t tid;
 
+  /* get executable file name in file_name string
+     so we can create thread name by executable file name. */
+  name = strtok_r(file_name, " ", &saveptr);
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
@@ -39,9 +44,9 @@ process_execute (const char *file_name)
   strlcpy (fn_copy, file_name, PGSIZE);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
-    palloc_free_page (fn_copy); 
+    palloc_free_page (fn_copy);
   return tid;
 }
 
