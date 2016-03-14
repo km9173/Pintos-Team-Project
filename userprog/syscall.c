4713bdbaa53b004eaa859f949f3e9625788b7f0c
GyeongMo Lim <km9173@naver.com>
2016-03-14 21:50:38 +0900

    add declare get_argument func in syscall.c

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -5,16 +5,22 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+void get_argument(void *esp, int *arg, int count);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+void get_argument (void *esp, int *arg, int count)
+{
+  printf("get argument func call!\n");
 }
