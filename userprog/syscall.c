b8e7e4270fa0a0b0f0e6e5f89f4ecb71c8f0f6ac
GyeongMo Lim <km9173@naver.com>
2016-03-28 01:21:36 +0900

    initialize system call funcs (open, filesize, read, write, seek, tell, close)

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -14,6 +14,13 @@ void halt (void);
 void exit (int status);
 bool create (const char *file, unsigned initial_size);
 bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
 
 void
 syscall_init (void)
@@ -192,7 +199,8 @@ remove (const char *file)
   return success;
 }
 
-int open (const char *file)
+int
+open (const char *file)
 {
   struct thread *t = thread_current();
   struct file *_file = filesys_open(file);
@@ -203,6 +211,42 @@ int open (const char *file)
 
   t->fd_table[fd] = _file;
   t->fd_size++;
-  
+
   return fd;
+}
+
+int
+filesize (int fd)
+{
+
+}
+
+int
+read (int fd, void *buffer, unsigned size)
+{
+
+}
+
+int
+write (int fd, void *buffer, unsigned size)
+{
+
+}
+
+void
+seek (int fd, unsigned position)
+{
+
+}
+
+unsigned
+tell (int fd)
+{
+
+}
+
+void
+close (int fd)
+{
+
 }
