9b038751a7b9ab32737f01ab7bf73debfb019b8b
GyeongMo Lim <km9173@naver.com>
2016-05-31 00:24:39 +0900

    Add swap_in func

--- vm/swap.c
+++ vm/swap.c
@@ -18,7 +18,7 @@ swap_init (size_t used_index, void* kaddr)
   lru_list_init ();
   swap_block = block_get_role (BLOCK_SWAP);
   swap_slot_bitmap =
-    bitmap_create (BLOCK_SECTOR_SIZE * block_size(swap_block) / PGSIZE);
+    bitmap_create (BLOCK_SECTOR_SIZE * block_size (swap_block) / PGSIZE);
   bitmap_set_all (swap_slot_bitmap, 0);
   lock_init (&swap_lock);
 }
@@ -26,7 +26,17 @@ swap_init (size_t used_index, void* kaddr)
 void
 swap_in (size_t used_index, void* kaddr)
 {
-
+  int i = 0;
+  lock_acquire (&swap_lock);
+  if (bitmap_test (swap_slot_bitmap, used_index))
+  {
+    bitmap_flip (swap_slot_bitmap, used_index);
+    for (i = 0; i < (PGSIZE / BLOCK_SECTOR_SIZE); i++)
+    {
+      block_read (swap_block, used_index * (PGSIZE / BLOCK_SECTOR_SIZE) + i, kaddr + i * BLOCK_SECTOR_SIZE);
+    }
+  }
+  lock_release (&swap_lock);
 }
 
 size_t
