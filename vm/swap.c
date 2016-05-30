9e94361b352f3a77c4d50393eba994d885e767c5
GyeongMo Lim <km9173@naver.com>
2016-05-31 00:57:36 +0900

    Modify swap_init func argument format

--- vm/swap.c
+++ vm/swap.c
@@ -13,7 +13,7 @@ static struct block *swap_block;
 static struct lock swap_lock;
 
 void
-swap_init (size_t used_index, void* kaddr)
+swap_init () // size_t used_index, void* kaddr
 {
   lru_list_init ();
   swap_block = block_get_role (BLOCK_SWAP);
