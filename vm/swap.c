76f974ddabfac2f55bf7a68ea4c6ab255539871b
GyeongMo Lim <km9173@naver.com>
2016-05-30 23:21:23 +0900

    Add reference link in swap.c

--- vm/swap.c
+++ vm/swap.c
@@ -1,5 +1,9 @@
 #include "swap.h"
 #include <bitmap.h>
+#include <block.h>
+#include "threads/vaddr.h"
+#include "threads/synch.h"
+#include "vm/frame.h"
 
 #define BLOCK_SIZE 512
 #define SLOT_SIZE 4096
@@ -15,8 +19,8 @@ swap_init (size_t used_index, void* kaddr)
   swap_block = block_get_role (BLOCK_SWAP);
   swap_slot_bitmap =
     bitmap_create (BLOCK_SECTOR_SIZE * block_size(swap_block) / PGSIZE);
-  bitmap_set_all(swap_slot_bitmap, 0);
-  lock_init(&swap_lock);
+  bitmap_set_all (swap_slot_bitmap, 0);
+  lock_init (&swap_lock);
 }
 
 void
