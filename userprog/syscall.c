78c7213125665e9a5a23bd9e5259c1fbb45b6aca
gigyun <gigyun@naver.com>
2016-06-26 19:32:38 +0900

    correct chdir to close previous directory

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -406,8 +406,8 @@ chdir (const char *dir)
     return false;
   else {
     /* 스레드의 현재 작업 디렉터리를 변경 */
+    dir_close (thread_current ()->cur_dir);
     thread_current ()->cur_dir = f_dir;
-    dir_close (f_dir);
     return true;
   }
 }
